1b3c1d7ae19445bb95cb381be8cfe37fba7344c2
Matt Wood <matt@cratebind.com>
2019-11-11 10:07:49 -0600

    Add webpack error overlay plugin, add stylesheet tag to app layout

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,11 @@
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
+
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const environment = require('./environment');
+
+environment.plugins.prepend('ErrorOverlayPlugin', new ErrorOverlayPlugin());
+
+environment.config.merge({ devtool: 'cheap-module-source-map' });
 
 module.exports = environment.toWebpackConfig();
