56547b658962f8eede640b42d4f29d75a0f0f286
Matt Wood <matt@cratebind.com>
2020-05-22 15:01:36 -0500

    Remove error overlay plugin

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,19 +1,16 @@
-const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const environment = require('./environment');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
-const environment = require('./environment');
-
-environment.plugins.prepend('ErrorOverlayPlugin', new ErrorOverlayPlugin());
 environment.plugins.prepend(
   'ReactRefreshWebpackPlugin',
   new ReactRefreshWebpackPlugin()
 );
 
-environment.config.merge({ devtool: 'cheap-module-source-map' });
+environment.config.merge({ devtool: 'source-map' });
 
 environment.module = {
   rules: [
